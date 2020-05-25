a8b60a6b62300757cbe31f46cf4fcd6cde6ca300
Yang Chen <chenyanghmilu@gmail.com>
2020-05-25 15:41:42 -0400

    practice demo mock admin panel

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ class App extends Component {
         <Sidebar currentUser={this.state.currentUser} />
         <MainPenel />
         <Switch>
-          <Route exact path='/' component={SignIn} />
+          <Route exact path='/login' component={SignIn} />
         </Switch>
       </div>
     );
