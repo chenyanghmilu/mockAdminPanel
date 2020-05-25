f6208c8a02fb92ada94e7179bc3bdc32291f9b35
Yang Chen <chenyanghmilu@gmail.com>
2020-05-25 15:26:37 -0400

    Updates

---
+++ precache-manifest.7836c4b26b8210d57463172dc7da6225.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a635f13c79d35e617c0dc70595e7067e",
+    "url": "/mockAdminPanel/index.html"
+  },
+  {
+    "revision": "3aa19cd705623a611675",
+    "url": "/mockAdminPanel/static/css/main.9aece84c.chunk.css"
+  },
+  {
+    "revision": "d3e6d1e414e9415c1b2a",
+    "url": "/mockAdminPanel/static/js/2.92f5c9d6.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/mockAdminPanel/static/js/2.92f5c9d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aa19cd705623a611675",
+    "url": "/mockAdminPanel/static/js/main.b4fb6825.chunk.js"
+  },
+  {
+    "revision": "1e6d7957fc083391478e",
+    "url": "/mockAdminPanel/static/js/runtime-main.17d582e8.js"
+  }
+]);
